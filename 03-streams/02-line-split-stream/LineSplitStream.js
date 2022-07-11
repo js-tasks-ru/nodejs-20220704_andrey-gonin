ccf2cedb1f4a1a387cbdb680f5114177ae24b990
AndreyGonin <andreychaki@gmail.com>
2022-07-12 00:07:47 +0500

    add solution for 03-streams/01-limit-size-stream and /02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,34 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.output = '';
   }
 
   _transform(chunk, encoding, callback) {
+
+    let strChunk = chunk.toString();
+    let temp = [];
+
+    if (strChunk.indexOf(os.EOL) < 0) {
+      this.output += strChunk;
+    } else {
+      temp = strChunk.split(os.EOL);
+
+      while (temp.length - 1) {
+        this.output += temp.shift()
+        this.push(this.output);
+        this.output = '';
+      }
+      this.output += temp.shift();
+    }
+
+    return callback();
   }
 
   _flush(callback) {
+    this.push(this.output);
+    callback();
   }
 }
 
