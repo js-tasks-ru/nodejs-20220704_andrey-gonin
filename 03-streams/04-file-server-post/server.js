21339db7c042e69cdaed8a4f9c78f0a4c4b9faee
AndreyGonin <andreychaki@gmail.com>
2022-07-20 00:01:44 +0500

    try 03-streams/04-file-server-post

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,9 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -10,10 +13,46 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+
   switch (req.method) {
-    case 'POST':
+    case 'POST': {
+
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end();
+        return;
+      }
+
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
+
+      const limitedStream = new LimitSizeStream({limit: 10000, encoding: 'utf-8'});
+
+      const stream = fs.createReadStream(filepath)
+        .pipe(req)
+        .pipe(limitedStream);
+
+      stream.on('error', err => {
+        res.statusCode = 413;
+        res.end();
+      });
+
+      fs.writeFile(filepath, stream.toString(), (err) => {
+        if (err) {
+          console.log('err', err);
+        }
+
+        res.statusCode = 201;
+        res.end();
+      });
+
 
       break;
+    }
+
 
     default:
       res.statusCode = 501;
