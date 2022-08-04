0da662367979f815105378b22de73329e9e34ced
AndreyGonin <andreychaki@gmail.com>
2022-08-04 23:07:12 +0500

    add solution for 06-mongodb-mongoose/02-rest-api

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,42 @@
+const Product = require('./../models/Product');
+const {mapProduct} = require('./../mappers/product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const {subcategory} = ctx.query;
 
-  if (!subcategory) return next();
+  const {subcategory} = ctx.request.query;
+  ctx.findPriductList = {};
 
-  ctx.body = {};
+  if (subcategory) {
+    if (!mongoose.Types.ObjectId.isValid(subcategory)) {
+      ctx.throw(400, 'invalid id');
+    }
+
+    ctx.findPriductList.subcategory = subcategory;
+  }
+
+  return next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find(ctx.findPriductList);
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.throw(400, 'invalid id');
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    ctx.throw(404, 'not found item');
+  }
+
+  ctx.body = {product: mapProduct(product)};
 };
 
